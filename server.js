3bfd14d5d911a5365cf4e3c60dab8748dae7263c
ljodice <>
2024-11-02 14:08:31 -0400

    added endpoints

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const database = new Datastore('database.db');
 database.loadDatabase();
 
-database.insert({"name":"Mike"})
+database.insert({"Testing_run":true})
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -22,12 +22,15 @@ app.listen(port, () => {
 });
 
 
-app.post('/schedule', (req,res) =>{
-  const data = req.body;
-  console.log(data)
-  database.insert(data);
-  res.json({
-    status: "200 Success"
-  })
+app.get("/schedule/member/:id", (req, res) => {});
+app.get("/schedule/team/:id", (req, res) =>{});
 
-})
+app.post('/schedule', (req,res) =>{
+  database.insert(req.body);
+  res.json({
+    status: "200 Success",
+    Data_Added: req.body
+  })
+});
+
+app.put("/schedule/update/:id", (req, res) => {})
